e1edaf641787652899bdc67593bd7ce67f71576c
grillopress <grillopress@gmail.com>
2017-11-22 19:47:57 +0000

    first commit

---
+++ routes/router.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render( 'index' );
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render( 'index' );
+});
+
+// GET v1 index page.
+router.get('/G', function(req, res, next) {
+  res.render( 'G/index' );
+});
+
+// GET all v1 URL reqs and push them to a template in the v1 file
+// This feels really brittle and hacky...
+// No handling of no view found...
+router.get('/G/:view', function(req, res, next) {
+  var theView = req.params.view;
+  res.render( 'G/' + theView );
+});
+
+router.get('/G/:subdir/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  res.render( 'G/' + theDir + '/' + theView );
+});
+
+router.get('/G/:subdir/:subdir2/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  var theDir2 = req.params.subdir2;
+  res.render( 'G/' + theDir + '/' + theDir2 + '/' + theView );
+});
+// // // // // // //
+
+module.exports = router;
