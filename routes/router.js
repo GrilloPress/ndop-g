efbc540e8ee15b9792d5a1f05223e5c5d2a74de8
Dan Johnston <daniel.johnston1@nhs.net>
2017-11-28 18:39:07 +0000

    Added initial bulk of content for Prototype G v3

--- routes/router.js
+++ routes/router.js
@@ -65,4 +65,32 @@ router.get('/v2/:subdir/:subdir2/:view', function(req, res, next) {
 });
 // // // // // // //
 
+// GET v1 index page.
+router.get('/v3', function(req, res, next) {
+  res.render( 'v3/index' );
+});
+
+// GET all v1 URL reqs and push them to a template in the v1 file
+// This feels really brittle and hacky...
+// No handling of no view found...
+router.get('/v3/:view', function(req, res, next) {
+  var theView = req.params.view;
+  res.render( 'v3/' + theView );
+});
+
+router.get('/v3/:subdir/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  res.render( 'v3/' + theDir + '/' + theView );
+});
+
+router.get('/v3/:subdir/:subdir2/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  var theDir2 = req.params.subdir2;
+  res.render( 'v3/' + theDir + '/' + theDir2 + '/' + theView );
+});
+
+// // // // // // //
+
 module.exports = router;
